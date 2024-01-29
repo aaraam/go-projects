dfcc39903c99a867496ebe5f1d3cbbb84301ce2f
aaraam <aaramblkice@gmail.com>
2024-01-29 15:12:58 +0530

    feat: added html form + api server

--- main.go
+++ main.go
@@ -1,110 +1,43 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
-
-	"errors"
-
-	"github.com/gin-gonic/gin"
 )
 
-type book struct {
-	ID       string `json:"id"`
-	Title    string `json:"title"`
-	Author   string `json:"author"`
-	Quantity int    `json:"quantity"`
-}
-
-var books = []book{
-	{ID: "1", Title: "Golang pointers", Author: "Mr. Golang", Quantity: 10},
-	{ID: "2", Title: "Goroutines", Author: "Mr. Goroutine", Quantity: 20},
-	{ID: "3", Title: "Golang routers", Author: "Mr. Router", Quantity: 30},
-}
-
-func getBooks(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, books)
-}
-
-func bookByID(c *gin.Context) {
-	id := c.Param("id")
-	book, err := getBookById(id)
-
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
 		return
 	}
-
-	c.IndentedJSON(http.StatusOK, book)
-}
-
-func checkoutBook(c *gin.Context) {
-	id, ok := c.GetQuery("id")
-
-	if ok == false {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Missing id query parameter"})
-	}
-
-	book, err := getBookById(id)
-
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
 		return
 	}
-
-	if book.Quantity <= 0 {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not available"})
-		return
-	}
-
-	book.Quantity -= 1
-	c.IndentedJSON(http.StatusOK, book)
+	fmt.Fprintf(w, "Hello!")
 }
 
-func returnBook(c *gin.Context) {
-	id, ok := c.GetQuery("id")
-
-	if ok == false {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Missing id query parameter"})
-	}
-
-	book, err := getBookById(id)
-
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
-
-	book.Quantity += 1
-	c.IndentedJSON(http.StatusOK, book)
-}
-
-func getBookById(id string) (*book, error) {
-	for i, b := range books {
-		if b.ID == id {
-			return &books[i], nil
-		}
-	}
-
-	return nil, errors.New("Book not found")
-}
-
-func createBook(c *gin.Context) {
-	var newBook book
-
-	if err := c.BindJSON(&newBook); err != nil {
-		return
-	}
-
-	books = append(books, newBook)
-	c.IndentedJSON(http.StatusCreated, newBook)
+	fmt.Fprintf(w, "POST request successful\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 }
 
 func main() {
-	router := gin.Default()
-	router.GET("/books", getBooks)
-	router.GET("/books/:id", bookByID)
-	router.POST("/books", createBook)
-	router.PATCH("/checkout", checkoutBook)
-	router.PATCH("/return", returnBook)
-	router.Run("localhost:8080")
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }

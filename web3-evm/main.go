745a838cee5d4e441bdce35b97092b8c41a9b430
aaraam <aaramblkice@gmail.com>
2024-02-01 16:02:50 +0530

    feat: added txn send function

--- web3-evm/main.go
+++ web3-evm/main.go
@@ -4,7 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/big"
 	"os"
+	"strings"
 	"web3-evm/pkg"
 
 	"github.com/joho/godotenv"
@@ -26,11 +28,34 @@ func handleBalance(balanceCmd *flag.FlagSet, address *string) {
 	fmt.Printf("Balance of %s: %s ETH\n", *address, balance)
 }
 
+func handleSend(sendCmd *flag.FlagSet, fromPrivKey *string, toAddress *string, amount *string) {
+	sendCmd.Parse(os.Args[2:])
+
+	if *fromPrivKey == "" || *toAddress == "" || *amount == "" {
+		fmt.Println("all arguments (fromPrivKey, toAddress, amount) are required")
+		sendCmd.PrintDefaults()
+		os.Exit(1)
+	}
+
+	amountBigInt, ok := new(big.Int).SetString(strings.Trim(*amount, "\""), 10)
+	if !ok {
+		fmt.Println("invalid amount")
+		os.Exit(1)
+	}
+
+	txHash, err := pkg.SendTransaction(*fromPrivKey, *toAddress, amountBigInt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Transaction sent! Tx Hash: %s\n", txHash)
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
 	balanceCmd := flag.NewFlagSet("balance", flag.ExitOnError)
 	balanceAddress := balanceCmd.String("address", "", "Ethereum address to fetch the balance")
 
@@ -40,11 +65,18 @@ func main() {
 		os.Exit(1)
 	}
 
+	sendCmd := flag.NewFlagSet("send", flag.ExitOnError)
+	fromPrivKey := sendCmd.String("from", "", "Private key of the sender")
+	toAddress := sendCmd.String("to", "", "Ethereum address to send to")
+	amount := sendCmd.String("amount", "", "Amount of Ether to send (in Wei)")
+
 	switch os.Args[1] {
 	case "balance":
 		handleBalance(balanceCmd, balanceAddress)
+	case "send":
+		handleSend(sendCmd, fromPrivKey, toAddress, amount)
 	default:
-		fmt.Println("expected 'balance' subcommand")
+		fmt.Println("expected 'balance' or 'send' subcommand")
 		os.Exit(1)
 	}
 }
